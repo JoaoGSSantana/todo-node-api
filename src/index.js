2b7966014b0343dc63244016d0b7a5f34fbe1681
João Gabriel Santana <joao.santana09@outlook.com>
2022-11-29 00:44:57 -0300

    finish todo node

--- src/index.js
+++ src/index.js
@@ -1,41 +1,164 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  try {
+    const { username } = request.headers;
+
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+      return response.status(404).json({ error: "User not found." });
+    }
+
+    request.user = user;
+
+    next();
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post("/users", (request, response) => {
+  try {
+    const { name, username } = request.body;
+
+    const usernameAlreadyExists = users.some(
+      (user) => user.username === username
+    );
+
+    if (usernameAlreadyExists) {
+      return response.status(400).json({ error: "User already exists!" });
+    }
+
+    const user = {
+      id: uuidv4(),
+      name,
+      username,
+      todos: [],
+    };
+
+    users.push(user);
+
+    return response.status(201).json({ ...user });
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  try {
+    const { user } = request;
+
+    return response.status(200).send(user.todos);
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  try {
+    const { user } = request;
+
+    const { title, deadline } = request.body;
+
+    const todo = {
+      id: uuidv4(),
+      title,
+      done: false,
+      deadline: new Date(deadline),
+      created_at: new Date(),
+    };
+
+    user.todos.push(todo);
+
+    return response.status(201).json({ ...todo });
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  try {
+    const { id } = request.params;
+    const { title, deadline } = request.body;
+
+    const { user } = request;
+
+    const todo = user.todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      return response.status(404).json({ error: "Todo not found." });
+    }
+
+    todo.title = title;
+    todo.deadline = new Date(deadline);
+
+    return response.status(201).json(todo);
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  try {
+    const { id } = request.params;
+    const { user } = request;
+
+    const todo = user.todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      return response.status(404).json({ error: "Todo not found." });
+    }
+
+    todo.done = true;
+
+    return response.status(201).json(todo);
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  try {
+    const { id } = request.params;
+    const { user } = request;
+
+    const todo = user.todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      return response.status(404).json({ error: "Todo not found." });
+    }
+
+    user.todos.splice(todo, 1);
+
+    return response.status(204).send();
+  } catch (error) {
+    return response.status(500).json({
+      error: "Not possible completed your request, please try again!",
+    });
+  }
 });
 
 module.exports = app;
